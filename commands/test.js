791c57f9729bc67a8d07ddc321156ec61667fc6a
intothefantasy <iwantoplayro2@gmail.com>
2018-04-21 02:43:35 +0800

    test

--- commands/test.js
+++ commands/test.js
@@ -7,7 +7,13 @@ module.exports = {
      if(isOwner){
       snekFetch.get("https://ws-tcg.com/todays-card/").then((result) => {
         let $ = cheerio.load(result.text);
-        console.log($('img[class=aligncenter]').attr('src'));
+        //console.log($('img[class=aligncenter]').attr('src'));
+        const img = [];
+
+        $('img[class=aligncenter]').each(function(i, elem) {
+          img[i] = $(this).attr('src');
+        });
+        console.log(img);
       });
      } else {
          return;
