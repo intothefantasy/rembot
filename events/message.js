f644b23e51ea543e69d61d01989f604863fee9f4
intothefantasy <iwantoplayro2@gmail.com>
2018-04-24 12:04:17 +0800

    test

--- events/message.js
+++ events/message.js
@@ -1,4 +1,19 @@
 const config = require("../config.json");
+const fs = require('fs');
+
+function checkFileOwner(filename){
+    fs.open("../commands/owner/"+filename+".js", 'r', (err, fd) => {
+  if (err) {
+    if (err.code === 'ENOENT') {
+      console.error('myfile does not exist');
+      return false;
+    }
+
+    throw err;
+  }
+    return true;
+});
+}
 
 module.exports = {
     run: (msg, Client) => {
@@ -14,6 +29,7 @@ module.exports = {
         let cmd = msg.content.split(" ")[0].substr(config.prefix.length);
         let args = msg.content.split(" ").slice(1);
         try {
+            console.log(checkFileOwner(cmd));
             require("../commands/" + cmd).run(args, Client, msg, isOwner);
         } catch (err) {
             console.log(err);
