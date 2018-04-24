c9739cfc9201dd2885602ab26e24c87aee931cf1
intothefantasy <iwantoplayro2@gmail.com>
2018-04-24 12:14:33 +0800

    test

--- events/message.js
+++ events/message.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 function checkFileOwner(filename){
     try
    {
-       return fs.statSync("../commands/owner/"+filename+".js").isFile();
+       return fs.statSync("./commands/owner/"+filename+".js").isFile();
    }
    catch (err)
    {
