7b0e1a62f4a0b6d7139f916f84b1855f09a7f188
intothefantasy <iwantoplayro2@gmail.com>
2018-04-24 12:57:05 +0800

    ttest

--- commands/owner/test.js
+++ commands/owner/test.js
@@ -25,7 +25,7 @@ module.exports = {
               }
             }
         }).then(function (message) {
-            msg.react(":question:");
+            msg.react("❓");
         });
 
           /*
