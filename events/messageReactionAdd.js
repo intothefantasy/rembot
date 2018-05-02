6e3289ee2cafd081a1d01a4db61b999be30bac5e
intothefantasy <iwantoplayro2@gmail.com>
2018-05-02 20:40:19 +0800

    test

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -2,9 +2,9 @@ const config = require("../config.json");
 
 module.exports = {
     run: (reaction, client) => {
-      //  if (reaction.emoji.name === "❓" && !reaction.message.author.bot) {
+        if (!reaction.message.author.bot) {
             console.log(reaction.users);
-      //  }
+        }
 
     }
 };
