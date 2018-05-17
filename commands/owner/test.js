ada804b9725854cb199497a3dce007fa3d3933ba
intothefantasy <iwantoplayro2@gmail.com>
2018-05-17 13:00:31 +0800

    t

--- commands/owner/test.js
+++ commands/owner/test.js
@@ -6,7 +6,7 @@ module.exports = {
   run : (args, client, msg, isOwner) => {
       if(isOwner){
 
-        console.log(client.users.get("name", "RoryRemMercury".toString()));
+        console.log(client.users.get("name", "RoryMercury".toString()));
         /*
           msg.channel.send({
             embed: {
