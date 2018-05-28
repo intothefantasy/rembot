1ce9575fc116ef1bdb05273826218d3ddf200bbc
intothefantasy <iwantoplayro2@gmail.com>
2018-05-28 09:35:12 +0800

    test

--- commands/owner/test.js
+++ commands/owner/test.js
@@ -10,8 +10,8 @@ module.exports = {
           snekFetch.get("https://yuyu-tei.jp/game_ws/carddetail/cardpreview.php?VER=lbext1.0&CID=10011&MODE=sell").then((result) => {
                let $ = cheerio.load(result.text);
                console.log($('.image_box').find('p[class=image] img').attr('src'));
-               console.log($('.price_box').find('p[class=price]').text());
-               console.log($('.price_box').find('p[class=stock]').text());
+               console.log($('.price_box').find('p[class=price]').text().replace(/\s+/g, ''));
+               console.log($('.price_box').find('p[class=stock]').text().replace(/\s+/g, ''));
           });
         /*
         msg.channel.send("<@"+config.ownerID+">");
